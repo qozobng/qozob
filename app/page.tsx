e93ec01859beafde42e65bfef7ab4db985d8fe62
Qozob <qozobng@gmail.com>
2026-03-07 08:16:34 +0100

    Fix TypeScript strict string error for Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ import {
 import { createClient } from '@supabase/supabase-js';
 
 // --- Supabase Setup ---
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // ---> PUT YOUR API KEY HERE <---
